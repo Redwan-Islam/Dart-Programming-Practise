785f207aa82b4b8e1aa331f9d5fdd8cc44102953
Redwan Islam <74811459+Redwan-Islam@users.noreply.github.com>
2022-01-08 11:30:04 +0600

    Add files via upload

---
+++ 06_string.dart
@@ -0,0 +1,46 @@
+void main() {
+  /*
+  //using single qotation;
+  String name = 'Using Single Qotation';
+  print(name);
+
+  //using double qotation;
+  String name1 = "Using Double Qotation";
+  print(name1);
+
+  //using var;
+  var name3 = 'declaring string using var';
+  print(name3);
+
+  //multiline string
+  var multipleString = ''' This is 
+  multiline String''';
+  print(multipleString);
+  
+  //Expression inside string;
+  var myName = 'Redwan Islam';
+  print('My name is $myName');
+
+  //string upperclass
+  var myName = 'Redwan Islam';
+  print('My name is ${myName.toUpperCase()}');
+
+  //string concatination
+  var f_Name = 'Redwan ';
+  var l_Name = 'Islam';
+  print(f_Name + l_Name);
+
+  //string Methods;
+  var name = 'RedwanIslam';
+  print(name.length);
+  print(name.isEmpty);
+  print(name.isNotEmpty);
+  print(name.contains(
+      'R')); // does my string has R or not if our string contains R then its return true otherwise it return false;
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+   */
+  //string split;
+  var name = 'Redwan Islam Dip';
+  print(name.split(' '));
+}
