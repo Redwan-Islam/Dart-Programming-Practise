4b1a4f41883a6006a488a6636eac3d18652870b4
Redwan Islam <74811459+Redwan-Islam@users.noreply.github.com>
2022-01-09 12:35:25 +0600

    Add files via upload

---
+++ 07_list.dart
@@ -0,0 +1,49 @@
+void main() {
+  //Growable List
+  /*
+  var myList = [1, 2, 3, 4, 5];
+  print(myList);
+  print(myList[0]);
+  print(myList.runtimeType);
+  */
+
+  //Strict List;
+  /*
+  var myList = <String>['Reahana', 'Mim', 'dip'];
+  print(myList);
+  print(myList.runtimeType);
+  */
+
+  //Spread Operator
+  /*
+  var maleStudents = ['sakib', 'tamim', 'redwan'];
+  var femaleStudents = ['susmita', 'rehana', 'tithi'];
+  var myList = [...maleStudents, ...femaleStudents];
+  print(myList);
+  */
+
+  //Empty List and adding values;
+  /*
+  var myList = [];
+  myList.add('redwan');
+  myList.add('rehana');
+  myList.add('mim');
+  print(myList);
+  print(myList.runtimeType);
+  */
+
+  //Empty List and adding values then remove
+  /*
+  var myList = [];
+  myList.add('redwan');
+  myList.add('rehana');
+  myList.add('mim');
+  print(myList);
+
+  //Remove values
+  myList.remove('mim'); //remove with values
+  myList.removeAt(1); // remove with index
+  print(myList);
+  print(myList.runtimeType);
+  */
+}
